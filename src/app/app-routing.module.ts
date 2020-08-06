4c315f14e78fab59adb94e56785f95df6f7d0633
Julien Broens <julien-broens@MBP-de-Julien.lan>
2020-08-06 15:58:05 +0200

    add import

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,11 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-
-const routes: Routes = [];
+import { SignupComponent } from './auth/signup/signup.component';
+import { SigninComponent } from './auth/signin/signin.component';
+const routes: Routes = [
+  { path: 'auth/signup', component: SignupComponent },
+  { path: 'auth/signin', component: SigninComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
