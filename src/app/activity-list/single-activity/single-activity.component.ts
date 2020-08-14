1b918037817950846c4feec76f555a5bf7e7c082
Julien Broens <julien-broens@MBP-de-Julien.lan>
2020-08-14 10:02:04 +0200

    add activity folder

---
+++ src/app/activity-list/single-activity/single-activity.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-single-activity',
+  templateUrl: './single-activity.component.html',
+  styleUrls: ['./single-activity.component.scss']
+})
+export class SingleActivityComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,6 +8,9 @@ import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { SignupComponent } from './auth/signup/signup.component';
 import { SigninComponent } from './auth/signin/signin.component';
+import { ActivityListComponent } from './activity-list/activity-list.component';
+import { ActivityFormComponent } from './activity-list/activity-form/activity-form.component';
+import { SingleActivityComponent } from './activity-list/single-activity/single-activity.component';
 
 @NgModule({
   declarations: [
@@ -15,6 +18,9 @@ import { SigninComponent } from './auth/signin/signin.component';
     HeaderComponent,
     SignupComponent,
     SigninComponent,
+    ActivityListComponent,
+    ActivityFormComponent,
+    SingleActivityComponent,
   ],
   imports: [
     BrowserModule,
