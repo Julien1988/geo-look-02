ec3022c626308eae712b2fb3cb76632493066846
Julien Broens <julien-broens@MBP-de-Julien.lan>
2020-08-14 09:59:19 +0200

    add isAuth

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,15 +1,28 @@
 import { Component, OnInit } from '@angular/core';
+import * as firebase from 'firebase';
+import { AuthService } from '../services/auth.service';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
-  styleUrls: ['./header.component.scss']
+  styleUrls: ['./header.component.scss'],
 })
 export class HeaderComponent implements OnInit {
+  isAuth: boolean;
 
-  constructor() { }
+  constructor(private authService: AuthService) {}
 
   ngOnInit(): void {
+    firebase.auth().onAuthStateChanged((user) => {
+      if (user) {
+        this.isAuth = true;
+      } else {
+        this.isAuth = false;
+      }
+    });
   }
 
+  onSignOut() {
+    this.authService.signOutUser();
+  }
 }
