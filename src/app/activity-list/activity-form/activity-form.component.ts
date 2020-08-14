40f9b3ee29deb8efc001c25b513723a8aaba9ce1
Julien Broens <julien-broens@MBP-de-Julien.lan>
2020-08-14 10:44:54 +0200

    add activity-list form html & ts

--- src/app/activity-list/activity-form/activity-form.component.ts
+++ src/app/activity-list/activity-form/activity-form.component.ts
@@ -1,15 +1,24 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl } from '@angular/forms';
 
 @Component({
   selector: 'app-activity-form',
   templateUrl: './activity-form.component.html',
-  styleUrls: ['./activity-form.component.scss']
+  styleUrls: ['./activity-form.component.scss'],
 })
 export class ActivityFormComponent implements OnInit {
+  constructor() {}
+  activityForm = new FormGroup({
+    ActivityTitle: new FormControl(''),
+    ActivityDescription: new FormControl(''),
+    ActivityDate: new FormControl(''),
+    ActivityType: new FormControl(''),
+  });
 
-  constructor() { }
+  ngOnInit(): void {}
 
-  ngOnInit(): void {
+  onSubmit() {
+    // TODO: Use EventEmitter with form value
+    console.warn(this.activityForm.value);
   }
-
 }
